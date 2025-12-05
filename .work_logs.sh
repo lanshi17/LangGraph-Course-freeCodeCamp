38411f3ce29c5b027b1187f6c68a7d161f9e3562
lanshi <yhvguk@foxmail.com>
2025-12-06 01:06:29 +0800

    ```plaintext feat(class_exercises): 完成 hello_word_graph.ipynb 示例并更新环境依赖
    - 在 class_exercises/hello_word_graph.ipynb 中添加 greeting_node 节点逻辑
    - 实现 graph 构建与调用流程，并展示执行结果
    - 更新 .work_logs.sh 添加 ipython 相关依赖安装记录
    ```

--- .work_logs.sh
+++ .work_logs.sh
@@ -3,3 +3,7 @@
 : 1764770459:0;uv venv
 : 1764770466:0;source .venv/bin/activate
 : 1764770474:0;uv add -r requirements.txt
+: 1764867505:0;source /home/lanshi/Documents/Graduate/01_Academics/14_LangGraph-Course/.venv/bin/activate
+: 1764868148:0;uv add Ipython
+: 1764868176:0;uv add Ipython.display
+: 1764868211:0;uv add ipython-display
